44d8025fec7316d94a9653cda80bfd6d855f5de0
mi nombre <desarrollowebaps@gmail.com>
2022-11-04 18:46:32 -0400

    04_visualDesigner

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+let btnResponsive = document.querySelector("#btnResponsive")
+let containerSection_navbar_ul = document.querySelector("#containerSection_navbar_ul")
+
+
+
+btnResponsive.addEventListener("click", () =>{ 
+        console.log("hola aqui estot")
+        containerSection_navbar_ul.classList.toggle("containerSection_navbar_ul_JS")
+        containerSection_navbar_ul.style.transition = "0.5s all ease"
+})
